583b80337660ca844563719f8b41f193f9cd621a
Rafael Robledo <xiDDdw@gmail.com>
2013-06-11 19:42:01 -0500

    Changed way to compute comparision between models.

--- cpp/hmm/main.cpp
+++ cpp/hmm/main.cpp
@@ -26,51 +26,48 @@ int main() {
 
   HMM h(data);
   
-	int max = h.MaxIterHMM();
+	int max = 2*h.MaxIterHMM();
 
 	int i, tid, nthreads;
 
 	// Auxiliar variables for omp process :)
-	double *ll1 = new double[max], 
-				 *ll2 = new double[max], 
+	double *ll = new double[max], 
 					mm1, mm2;
 
-	HMM::params *pp1 = new HMM::params[max], 
-							*pp2 = new HMM::params[max], 
+	HMM::params *pp = new HMM::params[max], 
 							 oo1, oo2;
 
 	clock_t ts = clock();
 
-	#pragma omp parallel for private(tid, i) shared(ll1, ll2, pp1, pp2) 
+	#pragma omp parallel for private(tid, i) shared(ll, pp) 
 	for(i=0; i<max; ++i) {
-		HMM::params p(nn, kk, true);
-		ll1[i] = h.EM(p);
-		pp1[i] = p;
-
-		HMM::params q(nn+1, kk, true);
-		ll2[i] = h.EM(q);
-		pp2[i] = q;
+		bool s = (i >= max/2);
+		HMM::params p(nn+s, kk, true);
+		ll[i] = h.EM(p);
+		pp[i] = p;
 
 		std::cout << "."; 
 	}
 
-	mm1 = ll1[0];
-	mm2 = ll2[0];
+	mm1 = -DBL_MAX;
+	mm2 = -DBL_MAX;
 
 	int ii1 = 0, ii2;
-	for(i=1; i<max; ++i) {
-		if(ll1[i] > mm1) {
-			mm1 = ll1[i];
+	for(i=0; i<max/2; ++i) {
+		if(ll[i] > mm1) {
+			mm1 = ll[i];
 			ii1 = i;
 		}
-
-		if(ll2[i] > mm2) {
-			mm2 = ll2[i];
+	}
+	for(; i<max; ++i) {
+		if(ll[i] > mm2) {
+			mm2 = ll[i];
 			ii2 = i;
 		}
 	}
-	oo1 = pp1[ii1];
-	oo2 = pp2[ii2];
+
+	oo1 = pp[ii1];
+	oo2 = pp[ii2];
 
 	clock_t te = clock();
 
@@ -81,8 +78,8 @@ int main() {
 
   auto res1 = oo1.hidden;
 	auto res2 = oo2.hidden;
-  writeCSV<uint>(res1, path + "pruebas\\cuervo1f_" + std::to_string(kk) + "mground.csv");
-	writeCSV<uint>(res2, path + "pruebas\\cuervo1f_" + std::to_string(kk+1) + "mground.csv");
+  writeCSV<uint>(res1, path + "pruebas\\cuervo1f_" + std::to_string(nn) + "_mground.csv");
+	writeCSV<uint>(res2, path + "pruebas\\cuervo1f_" + std::to_string(nn+1) + "_mground.csv");
 
   getchar();
   return 0;
